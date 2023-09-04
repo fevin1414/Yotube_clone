2c2c29f2c8551657a128b1dba5e2946bef897337
fevin1414 <fevinbiju1414@gmail.com>
2023-09-04 01:17:30 -0500

    added components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
+import {Navbar,Feed,ChannelDetail,SearchFeed,VideoDetail} from './components'
 
 function App() {
   return (
