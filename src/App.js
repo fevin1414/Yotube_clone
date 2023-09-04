3ca721fef639cb62f557cd2ca429e5a86ab947f6
fevin1414 <fevinbiju1414@gmail.com>
2023-09-04 01:08:57 -0500

    added app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+        <Box sx={{ backgroundColor: "#000" }}>
+         < Navbar />
+          <Routes>
+            <Route path="/" exact element={<Feed />} />
+            <Route path="/video/:id" exact element={<VideoDetail />} />
+            <Route path="/channel/:id" exact element={<ChannelDetail />} />
+            <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
+          </Routes>
+        </Box>
+      </BrowserRouter>
+    </div>
   );
 }
 
