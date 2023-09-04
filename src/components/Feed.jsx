73851476dda707c1c5f3696a66775de80aa34566
fevin1414 <fevinbiju1414@gmail.com>
2023-09-04 14:23:38 -0500

    added video

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,8 +3,19 @@ import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 import Sidebar from "./Sidebar";
 import Videos from "./Videos";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState(null);
+
+  useEffect(() => {
+    setVideos(null);
+
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [selectedCategory]);
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box
@@ -14,7 +25,10 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        <Sidebar />
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography
           className="copyright"
           variant="body2"
@@ -30,9 +44,9 @@ const Feed = () => {
           mb={2}
           sx={{ color: "white" }}
         >
-          New <span style={{ color: "#F31503" }}> Videos</span>
+         {selectedCategory}<span style={{ color: "#F31503" }}> Videos</span>
         </Typography>
-        <Videos videos={[]}/>
+        <Videos videos={[]} />
       </Box>
     </Stack>
   );
--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,22 +1,19 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const Base_URL='https://youtube-v31.p.rapidapi.com/captions';
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
 const options = {
-
-  url: Base_URL,
   params: {
-
-   maxResults:'50'
+    maxResults: 50,
   },
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-  }
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
 };
 
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
