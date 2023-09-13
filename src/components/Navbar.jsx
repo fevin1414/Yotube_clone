f055bd38b211d226ed494a6a8ce5db8973c4908f
fevin1414 <fevinbiju1414@gmail.com>
2023-09-13 17:21:07 -0500

    added home button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,9 @@ import { SearchBar } from "./";
 
 const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: 'white', top: 0, justifyContent: "space-between" }}>
-    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
-      <img src={logo} alt="logo" height={45} />
+    <Link to="/" style={{ display: "flex", alignItems: "center", color:"black" }}>
+      {/* <img src="../../public/images/logo1.png" alt="logo" height={45} /> */}
+      <h1>Home</h1>
     </Link>
     <SearchBar />
   </Stack>
