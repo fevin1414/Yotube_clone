622eb3bb566f904868d4a4c2cd5c78d567ea28c5
fevin1414 <fevinbiju1414@gmail.com>
2023-09-13 17:09:07 -0500

    fixed search bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,16 @@
-import React from "react";
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
-//import { logo } from "../utils/constants";
-import SearchBar from "./SearchBar";
 
-const Navbar = () => {
-  return (
-    <div>
-      <Stack
-        direction="row"
-        alignItems="center"
-        p={2}
-        sx={{
-          position: "sticky",
-          background: "white",
-          top: 0,
-          justifyContent: "space-between",
-        }}
-      >
-        <Link to="" style={{ display: "flex", alignItems: "center" }}>
-          <img src="../../public/images/logo1.png" alt="logo" height={45} />
-        </Link>
-        <SearchBar />
-      </Stack>
-    </div>
-  );
-};
+import { logo } from "../utils/constants";
+import { SearchBar } from "./";
+
+const Navbar = () => (
+  <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: 'white', top: 0, justifyContent: "space-between" }}>
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      <img src={logo} alt="logo" height={45} />
+    </Link>
+    <SearchBar />
+  </Stack>
+);
 
 export default Navbar;
