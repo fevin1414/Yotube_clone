7532059fde9ea0999f48ea463234ff7b89b90fbe
fevin1414 <fevinbiju1414@gmail.com>
2023-09-13 23:33:35 -0500

    final commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
 import { logo } from "../utils/constants";
@@ -7,8 +7,8 @@ import { SearchBar } from "./";
 const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: 'white', top: 0, justifyContent: "space-between" }}>
     <Link to="/" style={{ display: "flex", alignItems: "center", color:"black" }}>
-      { <img src={require('../images/logo1.png')} alt="logo" height={45} /> }
-      {/* <h1>Home</h1> */}
+      { <img src={require('../images/logo1.png')} alt="logo" height={45} width={100} /> }
+      {/* <h1>Home</h1> */}<Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "black",marginTop: "11px" ,background:"white"}}><span >Media</span></Typography>
     </Link>
     <SearchBar />
   </Stack>
