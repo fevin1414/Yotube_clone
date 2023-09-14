302f77b2356395ea100580d39268e356e242f18f
fevin1414 <fevinbiju1414@gmail.com>
2023-09-13 23:26:17 -0500

    added logo image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,8 @@ import { SearchBar } from "./";
 const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: 'white', top: 0, justifyContent: "space-between" }}>
     <Link to="/" style={{ display: "flex", alignItems: "center", color:"black" }}>
-      {/* <img src="../../public/images/logo1.png" alt="logo" height={45} /> */}
-      <h1>Home</h1>
+      { <img src={require('../images/logo1.png')} alt="logo" height={45} /> }
+      {/* <h1>Home</h1> */}
     </Link>
     <SearchBar />
   </Stack>
