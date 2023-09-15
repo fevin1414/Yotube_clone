f2ecf0e71245459e28fa12d8d25a0b12f1a7a85f
fevin1414 <fevinbiju1414@gmail.com>
2023-09-15 09:24:53 -0500

    added more styling

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -15,9 +15,9 @@ const SearchFeed = () => {
   }, [searchTerm]);
 
   return (
-    <Box p={2} minHeight="95vh">
-      <Typography variant="h4" fontWeight={900}  color="white" mb={3} ml={{ sm: "100px"}}>
-        Search Results for <span style={{ color: "#FC1503" }}>{searchTerm}</span> videos
+    <Box p={2} minHeight="95vh" backgroundColor="white">
+      <Typography variant="h4" fontWeight={900}  color="black" mb={3} ml={{ sm: "100px"}}>
+        Search Results for <span style={{ color: "black" }}>{searchTerm}</span> videos
       </Typography>
       <Box display="flex">
         <Box sx={{ mr: { sm: '100px' } }}/>
